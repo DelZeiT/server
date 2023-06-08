aee84d5f7c623ceedd790a94df17843f085f1fe0
DelZeiT <maksimcvetkov1106@gmail.com>
2023-06-08 13:39:05 +0300

    Добавил метод accept() для получения информации от клиента и сообщение о том, что клиент присоединился к чату.

--- server.py
+++ server.py
@@ -4,15 +4,17 @@ import socket
 clients = []    #список адресов клиеннтов
 flag = True
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((socket.gethostbyname(socket.gethostname()), 9090))     #назначаем адрес для сервера
 
-server.listen()        #прослушиваем указанный порт на получение сигналов
-
+server.listen(5)        #прослушиваем указанный порт на получение сигналов
 print('Сервер запущен!')
 
+conn, addr = server.accept()
+print(f'{addr}  - присоединился к чату')
+
 while flag:
-    message, addr = server.recvfrom(1024)     #не понял для чего. Вроде для получения данных от клиента в размере 1 Кб
+    message, addr = server.recv(1024).decode('utf-8')     #не понял для чего. Вроде для получения данных от клиента в размере 1 Кб
 
     if addr not in clients:                 #если нету адреса клиента в списке уже подключенных клиентов
         clients.append(addr)
@@ -20,7 +22,7 @@ while flag:
     print(message.decode('utf-8'))          #отправляем задекодированное сообщение
     for client in clients:
         if client != addr:                  #чтобы клиент не видел свое же сообщение
-            server.sendto(message, client)
+            server.send(message)
 
     if message == 'стоп':                   #если мы хотим остановить сервер
         flag = False
